d7cc289a57bb7f7726f5173cbd665752fbbf3a0f
AflaxCade <aflah.cade@gmail.com>
2025-02-01 11:49:45 +0300

    implemented edit profile functionality

--- a_core/urls.py
+++ a_core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from a_home.views import *
 
 urlpatterns = [
@@ -23,3 +25,7 @@ urlpatterns = [
     path('', home_view, name='home'),
     path('profile/', include('a_users.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- a_users/views.py
+++ a_users/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.views import redirect_to_login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from .forms import ProfileForm
 
 # Create your views here.
 
 def profile_view(request):
-        profile = request.user.profile
-        return render(request, 'a_users/profile.html', {'profile':profile})
+    profile = request.user.profile
+
+    
+
+    return render(request, 'a_users/profile.html', {'profile':profile})
+
+@login_required
+def profile_edit_view(request):
+    form = ProfileForm(instance=request.user.profile)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+
+    return render(request, 'a_users/profile_edit.html', {'form':form})  
