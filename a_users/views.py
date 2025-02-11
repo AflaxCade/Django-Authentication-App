11e7c918a6c68ef341f699f4637921f6c33c12ff
AflaxCade <aflah.cade@gmail.com>
2025-02-11 19:40:56 +0300

    added delete account functionality

--- a_users/views.py
+++ a_users/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from allauth.account.utils import send_email_confirmation
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth import logout
 from .forms import ProfileForm, EmailForm
 from django.urls import reverse
 
@@ -77,3 +78,15 @@ def profile_emailchange(request):
 def profile_emailverify(request):
     send_email_confirmation(request, request.user)
     return redirect('profile-settings')
+
+
+@login_required
+def profile_delete_view(request):
+    user = request.user
+    if request.method == "POST":
+        logout(request)
+        user.delete()
+        messages.success(request, 'Account deleted, what a pity')
+        return redirect('home')
+    
+    return render(request, 'a_users/profile_delete.html')
